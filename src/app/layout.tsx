40305fe0c7491999d858cb3438667e80606d86ad
email <md.shaquib0007@gmail.com>
2026-02-11 17:03:09 +0530

    updating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import "./globals.css";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({
   children,
 }: {
